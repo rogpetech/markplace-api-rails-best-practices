7c7ca13d1baa0f6fec15a5d08339fc44c81a52d2
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-07-25 09:37:18 -0300

    Add user first specs

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     email { FFaker::Internet.email }
     password { '123456' }
+    password_confirmation { '123456' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryBot.build(:user) }
+
+  subject { user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
 end

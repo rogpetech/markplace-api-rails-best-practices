263f0d4a8e6c6b235445b479197f3dee1a801723
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-07-30 19:31:43 -0300

    Adds update action the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1,
     constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :users, only: [:show, :create]
+      resources :users, only: %i[show create update]
     end
   end
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -55,4 +55,45 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
     end
   end
+
+  describe 'PUT/PATCH #update' do
+    context 'when is successfully updated' do
+      before(:each) do
+        @user = FactoryBot.create(:user)
+        patch :update, params: {
+          id: @user.id,
+          user: { email: 'newemail@skill.dev' }, format: :json
+        }
+      end
+
+      it 'renders the json representation for the updated user' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql('newemail@skill.dev')
+      end
+
+      it { should respond_with 200 }
+    end
+
+    context 'when is not updated' do
+      before(:each) do
+        @user = FactoryBot.create(:user)
+        patch :update, params: {
+          id: @user.id,
+          user: { email: 'bademail.com' }, format: :json
+        }
+      end
+
+      it 'renders an errors json' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on why the user could not be updated' do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include 'is invalid'
+      end
+
+      it { should respond_with 422 }
+    end
+  end
 end

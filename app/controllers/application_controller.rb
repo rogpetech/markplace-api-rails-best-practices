aa3f53d8a4bccce3edd69b6785d49dd2f89756de
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-10-16 20:21:54 -0300

    Add handler error to record_invalid

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,19 @@
 
 class ApplicationController < ActionController::API
   # protect_from_forgery with: :null_session
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
   include Authenticable
+
+  private
+
+  def record_not_found(exception)
+    Rails.logger.error "Not Found #{exception.message}"
+    render json: { error: exception.message }, status: :not_found
+  end
+
+  def record_invalid(exception)
+    Rails.logger.error "Record Invalid. #{exception}"
+    render json: { error: exception.message }, status: :unprocessable_entity
+  end
 end

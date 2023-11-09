ec7ea1edd7e3f735057bb827c9fa5d57e9206886
Rodolfo Peixoto <rodolfo.peixoto@rogpe.tech>
2023-11-09 15:14:35 -0300

    Adds the show action for order

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,8 +1,12 @@
 class Api::V1::OrdersController < ApplicationController
-  before_action :authenticate_with_token!, only: [:index]
+  before_action :authenticate_with_token!, only: [:index, :show]
   respond_to :json
 
   def index
     respond_with orders: current_user.orders
+  end
+
+  def show
+    respond_with(order: current_user.orders.find(params[:id]))
   end
 end
--- spec/controllers/api/v1/orders_controller_spec.rb
+++ spec/controllers/api/v1/orders_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::OrdersController, type: :controller do
-  describe "GET /index" do
+  describe 'GET /index' do
     before(:each) do
       current_user = FactoryBot.create(:user)
       api_authorization_header(current_user.token)
@@ -12,6 +12,22 @@ RSpec.describe Api::V1::OrdersController, type: :controller do
     it 'returns 4 order records from the user' do
       orders_response = json_response[:orders]
       expect(orders_response.count).to eq 4
+    end
+
+    it { should respond_with 200 }
+  end
+
+  describe '#GET #show' do
+    before(:each) do
+      current_user = FactoryBot.create(:user)
+      api_authorization_header(current_user.token)
+      @order = FactoryBot.create(:order, user: current_user)
+      get :show, params: { user_id: current_user.id, id: @order.id }
+    end
+
+    it 'returns the user order record matching o id' do
+      orders_response = json_response[:order]
+      expect(orders_response[:id]).to eql @order.id
     end
 
     it { should respond_with 200 }
